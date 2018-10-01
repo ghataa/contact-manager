fc7f1fc42e340955aad7bc64224fd1732a9074f4
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-10-01 23:32:57 +0200

    Add props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Contact from './components/Contact';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
       </div>
     );
   }
