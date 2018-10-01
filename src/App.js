73929ad0d08cec0f62434de4d868487e4dc63a78
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-10-02 00:09:15 +0200

    Style contact with bootstrap

--- src/App.js
+++ src/App.js
@@ -10,7 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
