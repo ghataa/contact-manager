2eb3d06d2efb17af2c00f76401d799d7d3a38895
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-10-02 00:02:31 +0200

    Style header with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Contact from "./components/Contact";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
