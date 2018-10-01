67932ce1512983481e8586e85e11855dc4e382d3
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-10-01 23:15:50 +0200

    Add Header component as dummy component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import Contact from './components/Contact';
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header />
         <Contact />
       </div>
     );
